de3f4e6895940dfd9534470ce4bb9db0fe693e3d
Laura Rodriguez <laurapra17@gmail.com>
2024-07-19 11:32:48 -0500

    modify the files x2

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,11 +10,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 app.use(bodyParser.json());
 
